b20ff6cf626d0e32b01c10f5bd94353802d005de
Yobany Benitez <x00360628@w..dom>
2020-03-30 15:52:38 -0500

    Checks if num2 is bigger than num1

--- HW2_Problem.py
+++ HW2_Problem.py
@@ -2,3 +2,5 @@ num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 if num1 > num2:
     print("num 1 is bigger than num 2")
+if num1 < num2:
+    print("num 1 is smaller than num 2")
